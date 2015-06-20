fb666f5f3af543ef25bfdf06266a3b18b55cb7e5
Fabian Löschner <w1th0utnam3@users.noreply.github.com>
2015-06-20 09:40:07 +0200

    Started implementing tests.

--- test/main.cpp
+++ test/main.cpp
@@ -19,74 +19,39 @@
 
 #include <iostream>
 #include <string>
+#include <cassert>
 
 #include "../source/undirected_graph.h"
 #include "../source/undirected_pair.h"
 #include "../source/graph_search_iterator.h"
 
-int main()
-{
+int run_test() {
 	typedef undirected_graph<std::string, size_t, double, undirected_pair<size_t>> graph_type;
 	graph_type graph;
-	std::string string1("Hello World!");
-	std::string string2("Testing some graphing.");
-	std::string string3("Another node.");
 
-	std::cout << "-Initial fill-" << std::endl;
-	graph.insert_vertex(1, string1);
-	graph.insert_vertex(2, string2);
-	graph.insert_vertex(3, string3);
+	const int n = 1000;
 
-	for(auto it = graph.begin_vertices(); it != graph.end_vertices(); ++it) {
-		std::cout << "Node " << it->first
-				 << "with value: " << it->second << std::endl;
+	std::vector<std::string> strings;
+	strings.reserve(n);
+
+	std::cout << "Inserting " << std::to_string(n) << " values... ";
+	for(int i = 0; i < n; i++) {
+		std::string string(std::to_string(std::sqrt(i)));
+		strings.push_back(string);
+		graph.insert_vertex(i, string);
 	}
+	std::cout << "ok. " << std::endl;
 
-	graph.insert_edge(1, 2, 44.12);
-	graph.insert_edge(2, 3, 22.34);
-	graph.insert_edge(3, 1, -111.21);
-
-	for(auto it = graph.begin_edges(); it != graph.end_edges(); ++it) {
-		std::cout << "Connection " << it->first.a << " to " << it->first.b << " with value: " << it->second << std::endl;
+	std::cout << "Checking inserted values... ";
+	for(int i = 0; i < n; i++) {
+		assert(graph.at_vertex(i).compare(strings.at(i)) == 0);
 	}
-
-	std::cout << "" << std::endl;
-	std::cout << "-BTS-" << std::endl;
-	breadth_first_iterator<graph_type> bts(graph, graph.begin_vertices());
-	while(!bts.end()) {
-		auto it = bts.next();
-		std::cout << it->first << std::endl;
-	}
-
-	std::cout << "" << std::endl;
-	std::cout << "-Erase vertex 2-" << std::endl;
-	graph.erase_vertex(2);
-
-	for(auto it = graph.begin_edges(); it != graph.end_edges(); ++it) {
-		std::cout << " Connection " << it->first.a << " to " << it->first.b << " with value: " << it->second << std::endl;
-	}
-
-	graph.insert_vertex(4, string2);
-	graph.insert_edge(1,4, 3521);
-	graph.insert_edge(3,4, 787435);
-
-	std::cout << "" << std::endl;
-	std::cout << "-Add edges-" << std::endl;
-	for(auto it = graph.begin_edges(); it != graph.end_edges(); ++it) {
-		std::cout << "Connection " << it->first.a << " to " << it->first.b << " with value: " << it->second << std::endl;
-	}
-
-	std::cout << "" << std::endl;
-	std::cout << "-Erase edge 4,1-" << std::endl;
-	graph.erase_edge(graph_type::make_edge_id(4,1));
-
-	for(auto it = graph.begin_edges(); it != graph.end_edges(); ++it) {
-		std::cout << " Connection " << it->first.a << " to " << it->first.b << " with value: " << it->second << std::endl;
-	}
-
-	std::cout << "" << std::endl;
-	std::cout << "-Value at edge 1,3-" << std::endl;
-	std::cout << graph.at_edge(graph_type::make_edge_id(1,3)) << std::endl;
+	std::cout << "ok." << std::endl;
 
 	return 0;
+}
+
+int main()
+{
+	return run_test();
 }
